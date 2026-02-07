974f7e7a65ddc67a5c4f68a79560706cba49a9c6
kaushik-72 <kaushikkarmakarsmart@gmail.com>
2026-02-07 08:25:59 +0530

    dom manp: id

--- notes.js
+++ notes.js
@@ -8,3 +8,22 @@ selectors
 -attribute name
 
 */
+
+//--------8 id-class-qs----------
+
+/*
+
+const el = document.getElementById("username");
+
+why we only use this "el" variable name? we can use any variable name but it is a convention to use "el" for element.
+
+what are the various properties of el?
+el.innerText
+el.classList
+el.id
+el.style
+el.parentElement
+el.children
+el.setAttribute("attributeName", "attributeValue")
+
+*/
